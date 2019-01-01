a2f53df140071c9bfc2d9ade5b483d3892cef576
Rachel Hawa <rachel.hawa@gmail.com>
2019-01-01 22:59:46 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,44 @@
+class Song
+  attr_accessor :name, :artist, :genre
+
+  @@count = 0
+  @@artists = []
+  @@genres  = []
+
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@count += 1
+    @@artists << @artist
+    @@genres << @genre
+  end
+
+  def self.count
+    @@count
+  end
+
+  def self.artists
+    @@artists.uniq
+  end
+
+  def self.genres
+    @@genres.uniq
+  end
+
+  def self.genre_count
+    genre_count = {}
+    @@genres.each do
+    |g| genre_count[g] = @@genres.count(g)
+  end
+    genre_count
+  end
+
+  def self.artist_count
+    artist_count = {}
+    @@artists.each do |a|
+    artist_count[a] = @@artists.count(a)
+    end
+    artist_count
+  end
+end
