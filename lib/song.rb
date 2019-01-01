6c50dc8690e7ab586e013b060c24c5428ddf2e0e
Rachel Hawa <rachel.hawa@gmail.com>
2019-01-01 22:38:02 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,8 +2,8 @@ class Song
   attr_accessor :name, :artist, :genre
 
   @@count = 0
-  @@artists 
-  @@genres
+  @@artists = []
+  @@genres  = []
 
   def initialize(name, artist, genre)
     @name = name
@@ -11,5 +11,5 @@ class Song
     @genre = genre
     @@count += 1
   end
-  
+
 end
